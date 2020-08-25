f2c8f5ddc7891b8f0c1bc319949fd227bca9b04a
Alex Bruns <ahb@Alexs-MacBook-Pro.local>
2020-08-24 23:59:16 -0500

    AHB

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,26 +6,7 @@ function Index() {
 
 export async function getStaticProps() {
   return {
-    props: {
-      data: {
-        theTwoNavyGuysReaderGroup: await (
-          await fetch(
-            "https://www.master-7rqtwti-hmyhm4xzoek6k.us-2.platformsh.site/the-twonavy-guys-reader-group-card"
-          )
-        ).json(),
-        theSpeculativeReadersGroup: await (
-          await fetch(
-            "https://www.master-7rqtwti-hmyhm4xzoek6k.us-2.platformsh.site/the-speculative-readers-group-card"
-          )
-        ).json(),
-        mainText: await (
-          await fetch(
-            "https://www.master-7rqtwti-hmyhm4xzoek6k.us-2.platformsh.site/landing-page-main-text"
-          )
-        ).json(),
-      },
-    },
-    revalidate: 15,
+    props: {},
   };
 }
 
