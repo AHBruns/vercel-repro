0520dc2dc4e54edeb5958a638852be562f8c03b3
Alex Bruns <ahb@Alexs-MacBook-Pro.local>
2020-08-25 00:06:07 -0500

    AHB

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { AppProps } from "next/app";
 import { useRouter } from "next/router";
 
 function App() {
   const router = useRouter();
+  console.log(`[${router.asPath}]`);
   return (
     <div>
       {router.asPath !== "/" && <div className="NO_SHOW" />}
